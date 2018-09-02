16fe6ffdda9f844bcd8d01d290f6f0cdd36ae25d
Kirill Kholodilin <chrl@users.noreply.github.com>
2018-09-02 22:26:23 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-go build
-VERSION=`cat bottalk-cli.go | grep 'const Version = ' | awk '{print $4}'`
-echo "Publishing version ${VERSION}"
-scp ./bottalk-cli frontend.bt:/home/deploy/console/console/binary.${VERSION}.bin
-echo "{\"version\":${VERSION}}" > version.json
-scp ./version.json frontend.bt:/home/deploy/console/console/version.json
-rm -rf ./version.json
