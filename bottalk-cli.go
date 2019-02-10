54efac0521c177448e65886c873792038ea73785
Kirill Kholodilin <kirill.kholodilin@real-digital.de>
2019-02-10 20:49:00 +0100

    Add main cli

--- bottalk-cli.go
+++ bottalk-cli.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -13,15 +14,30 @@ const Version = 19
 
 var path string
 
+type Token struct {
+	Token   string `json:"access_token"`
+	Expires int    `json:"expires_in"`
+	Refresh string `json:"refresh_token"`
+	Scope   string `json:"scope"`
+}
+
+var token Token
+
 func main() {
 	bottalkCli := cli.NewApp()
 	bottalkCli.Name = "bottalk"
 	bottalkCli.Usage = "CLI helper for Bottalk.de"
 	bottalkCli.Description = ""
 	bottalkCli.Version = fmt.Sprintf("%d", Version)
+
 	bottalkCli.Commands = append(
 		bottalkCli.Commands,
 		getTestCommands()...,
+	)
+
+	bottalkCli.Commands = append(
+		bottalkCli.Commands,
+		getLoginCommands()...,
 	)
 
 	bottalkCli.Flags = []cli.Flag{
@@ -34,10 +50,23 @@ func main() {
 		},
 	}
 
+	loadToken()
+
 	checkUpdate()
 
 	err := bottalkCli.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func loadToken() bool {
+	f, _ := os.Open(userHomeDir() + "/.bottalk-cli.token")
+	j := json.NewDecoder(f)
+	err := j.Decode(&token)
+	if err != nil {
+		log.Println("Wrong data in tokenfile: " + err.Error())
+		return false
+	}
+	return true
 }
